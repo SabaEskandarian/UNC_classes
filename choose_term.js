ccc0dfaf325b455885d9ffc0e597dac1d4d4e113
saba <saba.eskandarian@gmail.com>
2023-10-05 13:59:58 -0400

    update to 2024

--- choose_term.js
+++ choose_term.js
@@ -6,7 +6,7 @@ document.write("<form>\n");
 document.write("<label for='terms'>Choose a term: </label>\n");
 document.write("<select name='terms' onchange='redirect(this.value)'>\n");
 document.write("<option value=''>Choose Term</option>\n");
-document.write("<option value='spring2023'>Spring 2023</option>\n");
+document.write("<option value='spring2024'>Spring 2024</option>\n");
 document.write("<option value='fall2023'>Fall 2023</option>\n");
 document.write("<option value='summerII2023'>Summer II 2023</option>\n");
 document.write("<option value='summerI2023'>Summer I 2023</option>\n");
